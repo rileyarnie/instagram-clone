c1722acf843236bb7d2fd775db9d5bb82dad1292
Arnold Kibira <arnold.ril3y@gmail.com>
2020-07-31 22:41:10 +0300

    create user database model

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { string } = require("@hapi/joi");
+
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
