07bae1a8c9ba0c89adec9baabe878f30e07944b3
Arnold Kibira <arnold.ril3y@gmail.com>
2020-07-31 17:01:52 +0300

    define new post route and configure multer to handle multipart form data

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,41 @@
 const express = require("express");
+const multer = require("multer");
+const createError = require("http-errors");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(createError.BadRequest("Chosen filetype not supported"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 1024 * 1024 * 3 },
+  fileFilter,
+});
 
 const router = express.Router();
 const postsController = require("../controllers/postsController");
 
 router.get("", postsController.getPosts);
-router.post("/create-post", postsController.createPost)
+router.post(
+  "/create-post",
+  upload.single("imageUrl"),
+  postsController.createPost
+);
 
 module.exports = router;
