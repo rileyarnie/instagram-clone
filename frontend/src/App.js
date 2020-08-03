2a2c12192aa902f952a45e3c8e29d6bbeef27c26
Arnold Kibira <arnold.ril3y@gmail.com>
2020-08-03 21:34:04 +0300

    connect to redux store and call actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Post from "./components/Post/Post";
+import * as actionTypes from "./store/actions/actionTypes";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.postReducer.posts);
+  useEffect(() => {
+    dispatch(actionTypes.gettingPosts());
+  }, [dispatch]);
+
   return (
     <div>
       <Navigation />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map((post) => (
+        <Post
+          caption={post.caption}
+          creator={post.creator}
+          imageUrl={post.imageUrl}
+          key={post._id}
+          comments={post.comments}
+        />
+      ))}
     </div>
   );
 }
