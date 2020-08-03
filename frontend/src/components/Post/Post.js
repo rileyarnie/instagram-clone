65f75c9f3907afacba2b934a97c29ea970bffd0a
Arnold Kibira <arnold.ril3y@gmail.com>
2020-08-03 21:28:48 +0300

    add new comment styling

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -2,42 +2,56 @@ import React from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
 
-const Post = () => {
+const Post = ({ comments, creator, caption, imageUrl }) => {
   return (
     <div className="post">
       <div className="post__header">
         <div className="post__creator">
           <Avatar alt="Riley" src="profilepic"></Avatar>
-          <p className="post__username">username</p>
+          <p className="post__username">{creator.username}</p>
         </div>
         <div className="post__options">
-          <img src="https://img.icons8.com/ios-filled/24/000000/more.png" alt="post" />
+          <img
+            src="https://img.icons8.com/ios-filled/24/000000/more.png"
+            alt="post"
+          />
         </div>
       </div>
       <div className="post__body">
-        <img
-          className="post__image"
-          src="https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:196958?height=729&width=1296&format=jpg&quality=.7"
-          alt="oy"
-          name
-        />
+        <img className="post__image" src={imageUrl} alt="oy" />
+
+        {caption ? (
+          <div className="post__comments">
+            <p className="post__comment">
+              <span className="post__comment-username">{creator.username}</span>{" "}
+              {caption}
+            </p>
+          </div>
+        ) : (
+          ""
+        )}
+
         <div className="post__comments">
-          <p className="post__comment">
+          <p className="post__comments_title">comments</p>
+          {comments.map((comment) => (
+            <p className="post__comment" key={comment._id}>
+              <span className="post__comment-username">
+                {comment.author.username}
+              </span>{" "}
+              {comment.content}
+            </p>
+          ))}
+          {/* <p className="post__comment">
             <span className="post__comment-username">Username</span> Lorem ipsum
             dolor sit amet, consectetur adipiscing elitconsectetur adipiscing
             elitconsectetur adipiscing elit
-          </p>
-          <p className="post__comment">
-            <span className="post__comment-username">Username</span> Lorem ipsum
-            dolor sit amet, consectetur adipiscing elitconsectetur adipiscing
-            elitconsectetur adipiscing elit
-          </p>
+          </p> */}
         </div>
       </div>
       <div className="post__footer">
         <form className="post__form">
           <input className="post__input" placeholder="Add a comment..." />
-          <button class="post__button">Post</button>
+          <button className="post__button">Post</button>
         </form>
       </div>
     </div>
