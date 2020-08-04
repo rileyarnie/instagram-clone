b8bd9e39a733477b4dbb5369d265385131ff0b53
Riley <riley@pop-os.localdomain>
2020-08-04 18:33:35 +0300

    add toast to ender the error on the UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,50 @@
 import React, { useEffect } from "react";
 import "./App.css";
+
 import Navigation from "./components/Navigation/Navigation";
 import Post from "./components/Post/Post";
 import * as actionTypes from "./store/actions/actionTypes";
 import { useDispatch, useSelector } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.postReducer.posts);
+  const error = useSelector((state) => state.postReducer.error);
+
   useEffect(() => {
     dispatch(actionTypes.gettingPosts());
-  }, [dispatch]);
+    toast.error(error);
+  }, [dispatch, error]);
 
   return (
     <div>
       <Navigation />
-      {posts.map((post) => (
-        <Post
-          caption={post.caption}
-          creator={post.creator}
-          imageUrl={post.imageUrl}
-          key={post._id}
-          comments={post.comments}
+      {error ? (
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
         />
-      ))}
+      ) : (
+        posts.map((post) => (
+          <Post
+            caption={post.caption}
+            creator={post.creator}
+            imageUrl={post.imageUrl}
+            key={post._id}
+            _id={post._id}
+            comments={post.comments}
+          />
+        ))
+      )}
     </div>
   );
 }
