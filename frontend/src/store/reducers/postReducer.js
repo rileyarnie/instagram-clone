186ee6a1086649548a10fd2bd860a799f7fa0cf9
Riley <riley@pop-os.localdomain>
2020-08-04 18:36:19 +0300

    add comment reducer

--- frontend/src/store/reducers/postReducer.js
+++ frontend/src/store/reducers/postReducer.js
@@ -2,12 +2,19 @@ import * as actionTypes from "../actions/actions";
 
 const initialState = {
   posts: [],
+  error: null,
 };
 
 const postReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.GET_POSTS:
       return { ...state, posts: action.payload };
+
+    case actionTypes.GET_POSTS_ERROR:
+      return { ...state, error: action.payload };
+
+    case actionTypes.POST_COMMENT:
+      return { ...state };
 
     default:
       return state;
