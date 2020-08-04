f5a93af8186efc42781535f87a26e839c9156860
Arnold Kibira <arnold.ri3ey@gmail.com>
2020-08-04 18:52:08 +0300

    define action type constants

--- frontend/src/store/actions/actions.js
+++ frontend/src/store/actions/actions.js
@@ -1,2 +1,12 @@
+//AUTH CONSTANTS
+export const AUTH_START = "AUTH_START";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL";
+export const AUTH_LOGOUT = "AUTH_LOGOUT";
+
+//POSTS CONSTANTS
 export const GET_POSTS = "GET_POSTS";
+export const GET_POSTS_ERROR = "GET_POSTS_ERROR";
+export const POST_COMMENT = 'POST_COMMENT'
+
 
