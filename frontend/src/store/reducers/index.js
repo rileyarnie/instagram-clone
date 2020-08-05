dd8aeab4af192e723e8f20f562214d3bdb58c343
Arnold Kibira <arnold.ri3ey@gmail.com>
2020-08-05 22:07:06 +0300

    combine all reducers

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import postReducer from "./postReducer";
+import authReducer from "./authReducer";
 
 const rootReducer = combineReducers({
   postReducer,
+  authReducer,
 });
 
 export default rootReducer;
