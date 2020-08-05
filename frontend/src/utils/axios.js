8f77a366080e1f7d0657a4c7b895c93b5ce5d9e4
Arnold Kibira <arnold.ri3ey@gmail.com>
2020-08-05 22:07:38 +0300

    add authorization header

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+const token = localStorage.getItem("access_token");
+
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000",
   headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMjZmYmU3N2QzNjIzNGI1ZWUyY2ExNCIsInVzZXJuYW1lIjoicmlsZXkiLCJpYXQiOjE1OTYzOTI4NDIsImV4cCI6MTU5NjM5NDY0Mn0.8mBNb-LS38EaoMlsKyFgloLVVWE96kjJVHSY0s6aVlE",
+    Authorization: `Bearer ${token}`,
   },
 });
 
