6d3aafeae479a73828b95dd802c602f176677de0
Arnold Kibira <arnold.ri3ey@gmail.com>
2020-08-05 22:06:17 +0300

    handle the different authentication cases

--- frontend/src/store/reducers/authReducer.js
+++ frontend/src/store/reducers/authReducer.js
@@ -10,20 +10,46 @@ const initialState = {
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
-      return { ...state, loading: true };
+      return {
+        ...state,
+        isAuthenticated: false,
+        token: null,
+        error: null,
+        loading: true,
+      };
     case actionTypes.AUTH_SUCCESS:
       return {
         ...state,
         isAuthenticated: true,
         token: action.payload,
         loading: false,
+        error: null,
       };
     case actionTypes.AUTH_FAIL:
-      return { ...state, error: action.payload };
+      return {
+        ...state,
+        isAuthenticated: false,
+        token: null,
+        loading: false,
+        error: action.payload,
+      };
     case actionTypes.AUTH_LOGOUT:
-      return state;
+      return {
+        ...state,
+        isAuthenticated: false,
+        token: null,
+        loading: false,
+        error: null,
+      };
+    case actionTypes.CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
 
     default:
       return state;
   }
 };
+
+export default authReducer;
