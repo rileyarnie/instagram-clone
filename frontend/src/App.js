309c3e1151e2c0dfb44f2225368214954e308bf8
Arnold Kibira <arnold.ri3ey@gmail.com>
2020-08-05 21:58:14 +0300

    add dispatch method that runs every time the app is loaded to check the if the user is authenticated and if token is valid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,24 @@
 import React, { useEffect } from "react";
 import "./App.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Navigation from "./components/Navigation/Navigation";
-import Post from "./components/Post/Post";
 import * as actionTypes from "./store/actions/actionTypes";
-import { useDispatch, useSelector } from "react-redux";
-import { ToastContainer, toast } from "react-toastify";
+import { useDispatch } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
-import { Link, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import PostList from "./components/PostList/PostList";
 
 function App() {
   const dispatch = useDispatch();
-  // const posts = useSelector((state) => state.postReducer.posts);
-  const error = useSelector((state) => state.postReducer.error);
 
   useEffect(() => {
-    dispatch(actionTypes.gettingPosts());
-    toast.error(error);
-  }, [dispatch, error]);
+    dispatch(actionTypes.checkAuthState());
+  }, [dispatch]);
 
   return (
     <div>
-      {error ? (
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      ) : (
-        ""
-      )}
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
