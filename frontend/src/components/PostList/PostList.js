0fb252c335d8f7c135f9d698ef58ad9319989b45
Arnold Kibira <arnold.ri3ey@gmail.com>
2020-08-05 22:03:04 +0300

    add post list components and fetch posts on mount

--- frontend/src/components/PostList/PostList.js
+++ frontend/src/components/PostList/PostList.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Post from "../Post/Post";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import * as actionTypes from "../../store/actions/actionTypes";
+import { useHistory } from "react-router-dom";
 
 const PostList = () => {
   const posts = useSelector((state) => state.postReducer.posts);
-  //   const posts = useSelector((state) => state.postReducer.posts);
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state.postReducer.error);
+  const history = useHistory();
+
+  const isAuthenticated = useSelector(
+    (state) => state.authReducer.isAuthenticated
+  );
+
+  useEffect(() => {
+    isAuthenticated ? history.push("/") : history.push("/login");
+  }, [isAuthenticated, history]);
+
+  useEffect(() => {
+    dispatch(actionTypes.gettingPosts());
+  }, [dispatch]);
+
+  useEffect(() => {
+    toast.error(error);
+  }, [error]);
   return (
     <div>
+      {error ? (
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      ) : (
+        ""
+      )}
       {posts.map((post) => (
         <Post
           caption={post.caption}
